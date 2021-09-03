944266fb762fe6a82c342678dc33a02bbe9866e8
y091886 <aniko.nagy-I@generali.com>
2021-09-03 12:29:36 +0200

    create-db drop-db

---
+++ drop-db.sql
@@ -0,0 +1,7 @@
+EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'BikeStores'
+GO
+USE [master]
+GO
+/****** Object:  Database [BikeStores]    Script Date: 2021.09.03 12:27:34 ******/
+DROP DATABASE [BikeStores]
+GO
